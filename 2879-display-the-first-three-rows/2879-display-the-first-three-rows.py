8bb7a15161b0f06b77fc3305c09bd89ff9d8ad6b
Pavanakumar Devadiga <pavanakumarharigadde@gmail.com>
2026-08-30 12:45:05 +0530

    Solved a problem Time: 270 ms (74.44%), Space: 66.2 MB (80.86%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 2879-display-the-first-three-rows/2879-display-the-first-three-rows.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    return employees[0:3]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
