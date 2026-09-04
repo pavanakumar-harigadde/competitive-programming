ed208ca19596ddb6a1b0905706fc9a0b1c1bc869
Pavanakumar Devadiga <pavanakumarharigadde@gmail.com>
2026-09-04 23:59:13 +0530

    Time: 13 ms (53.09%), Space: 12.4 MB (60.49%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        for i in range(len(nums)):
+            prev = nums[:i+1]
+            next = nums[i:]
+            if (max(prev)-min(next)) <= k :
+                return i
+        
+        return -1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
