7fade0b13dfb7eaa100187ed7976481a77b91099
Pavanakumar Devadiga <pavanakumarharigadde@gmail.com>
2026-09-13 22:12:25 +0530

    1929. Concatenation of Array Time: 0 ms (100%), Space: 12.4 MB (99.86%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return 2*nums
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
