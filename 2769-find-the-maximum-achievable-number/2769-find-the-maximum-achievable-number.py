4696ba266b506f32cdb917aa73004c837a5f140b
Pavanakumar Devadiga <pavanakumarharigadde@gmail.com>
2026-09-14 21:46:27 +0530

    2769. Find the Maximum Achievable Number Time: 0 ms (100%), Space: 12.5 MB (15.42%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        return num + 2*t
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
