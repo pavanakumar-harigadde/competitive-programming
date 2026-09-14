486751dc9c27c32a499fa022b6a5f60b59754183
Pavanakumar Devadiga <pavanakumarharigadde@gmail.com>
2026-09-14 21:47:22 +0530

    2894. Divisible and Non-divisible Sums Difference Time: 3 ms (60.54%), Space: 19.4 MB (19.55%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.py
@@ -0,0 +1,15 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1=0
+        num2=0
+        for i in range(1,n+1):
+            if i%m!=0:
+                num1+=i
+            else:
+                num2+=i
+
+        return num1-num2
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
