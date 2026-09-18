29455b40a4cabbacf2602bb17e45798b065ee2bb
Pavanakumar Devadiga <pavanakumarharigadde@gmail.com>
2026-09-18 11:50:38 +0530

    2942. Find Words Containing Character Time: 3 ms (29.46%), Space: 19.2 MB (89.62%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        return [i for i in range(len(words)) if x in words[i]]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
