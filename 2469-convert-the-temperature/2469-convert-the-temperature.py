e2bf4a84a33e7173f1db0d7b81629f9b9399b905
Pavanakumar Devadiga <pavanakumarharigadde@gmail.com>
2026-09-23 22:50:07 +0530

    2469. Convert the Temperature Time: 0 ms (100%), Space: 19.4 MB (15.7%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -0,0 +1,7 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        return [celsius+273.15 , celsius*1.8+32.0]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
